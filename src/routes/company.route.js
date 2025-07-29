cdd7a574dbebb35a3b33d78687a655d470e84b30
jzaragosa06 <junjunzaragosa2309@gmail.com>
2025-07-29 10:39:32 +0800

    fix: fix company routing
    add a preceeding words before the id

--- src/routes/company.route.js
+++ src/routes/company.route.js
@@ -15,43 +15,43 @@ router.get('/:company_id', companyController.getCompany);
 //company-teams
 router.post('/:company_id/teams', companyTeamController.create); //create new company team
 router.get('/:company_id/teams', companyTeamController.getAll); //get all company team
-router.get('/:company_id/:team_id', companyTeamController.getOne);
-router.patch('/:company_id/:team_id', companyTeamController.update);
-router.delete('/:company_id/:team_id', companyTeamController.delete);
+router.get('/:company_id/teams/:team_id', companyTeamController.getOne);
+router.patch('/:company_id/teams/:team_id', companyTeamController.update);
+router.delete('/:company_id/teams/:team_id', companyTeamController.delete);
 
 //company-office
 router.post('/:company_id/offices', companyOfficeController.create); //create new company office
 router.get('/:company_id/offices', companyOfficeController.getAll); //get all company offices
-router.get('/:company_id/:office_id', companyOfficeController.getOne);
-router.patch('/:company_id/:office_id', companyOfficeController.update);
-router.delete('/:company_id/:office_id', companyOfficeController.delete);
+router.get('/:company_id/offices/:office_id', companyOfficeController.getOne);
+router.patch('/:company_id/offices/:office_id', companyOfficeController.update);
+router.delete('/:company_id/offices/:office_id', companyOfficeController.delete);
 
 //company-departments
 router.post('/:company_id/deparments', companyDepartmentController.create); //create new department
 router.get('/:company_id/departments', companyDepartmentController.getAll); //get all departmenet
-router.get('/:company_id/:department_id', companyDepartmentController.getOne);
-router.patch('/:company_id/:department_id', companyDepartmentController.update);
-router.delete('/:company_id/:department_id', companyDepartmentController.delete);
+router.get('/:company_id/deparments/:department_id', companyDepartmentController.getOne);
+router.patch('/:company_id/deparments/:department_id', companyDepartmentController.update);
+router.delete('/:company_id/deparments/:department_id', companyDepartmentController.delete);
 
 //company-division
 router.post('/:company_id/divisions', companyDivisionController.create); //create new department
 router.get('/:company_id/division', companyDivisionController.getAll); //get all departmenet
-router.get('/:company_id/:division_id', companyDivisionController.getOne);
-router.patch('/:company_id/:division_id', companyDivisionController.update);
-router.delete('/:company_id/:division_id', companyDivisionController.delete);
+router.get('/:company_id/divisions/:division_id', companyDivisionController.getOne);
+router.patch('/:company_id/divisions/:division_id', companyDivisionController.update);
+router.delete('/:company_id/divisions/:division_id', companyDivisionController.delete);
 
 //company-job-titles
 router.post('/:company_id/jobs', companyJobTitleController.create); //create new job title
 router.get('/:company_id/jobs', companyJobTitleController.getAll); //get all job title
-router.get('/:company_id/:job_title_id', companyJobTitleController.getOne);
-router.patch('/:company_id/:job_title_id', companyJobTitleController.update);
-router.delete('/:company_id/:job_title_id', companyJobTitleController.delete);
+router.get('/:company_id/jobs/:job_title_id', companyJobTitleController.getOne);
+router.patch('/:company_id/jobs/:job_title_id', companyJobTitleController.update);
+router.delete('/:company_id/jobs/:job_title_id', companyJobTitleController.delete);
 
 //company-industries
 router.post('/:company_id/job-industries', companyIndustryController.create); //create new job industry
 router.get('/:company_id/job-industries', companyIndustryController.getAll); //get all job industry
-// router.get('/:company_id/:industry_id',companyIndustryController.);
-router.patch('/:company_id/:industry_id', companyIndustryController.update);
-router.delete('/:company_id/:industry_id', companyIndustryController.delete);
+// router.get('/:company_id/job-industries/:industry_id',companyIndustryController.);
+router.patch('/:company_id/job-industries/:industry_id', companyIndustryController.update);
+router.delete('/:company_id/job-industries/:industry_id', companyIndustryController.delete);
 
 module.exports = router; 
