cb0c7d061f326ef78d0217b5e0e865cb22e6a11e
jzaragosa06 <junjunzaragosa2309@gmail.com>
2025-09-03 13:30:49 +0800

    chore: install mysql2
    manually install mysql2 to fix deployment error

--- src/index.js
+++ src/index.js
@@ -1,10 +1,3 @@
-// const app = require("./app");
-// const env = require("./config/env");
-
-// app.listen(env.VITE_FRONTEND_PORT, () => {
-//     console.log(`HRIS Employee Management runing on port: ${env.VITE_FRONTEND_PORT}: access on localhost:${env.VITE_FRONTEND_PORT}`)
-// });
-
 const app = require("./app");
 
 module.exports = app;
