d4bc045978c82373796756d31bec4e8f7218e1a9
jzaragosa06 <junjunzaragosa2309@gmail.com>
2025-09-03 15:06:09 +0800

    chore: export app and allow listening

--- src/config/db.js
+++ src/config/db.js
@@ -1,27 +1,3 @@
-// const { Sequelize } = require("sequelize");
-// const env = require("./env");
-
-// const sequelize = new Sequelize(
-//     env.DB_NAME_DEV,
-//     env.DB_USER,
-//     env.DB_PASSWORD,
-//     {
-//         host: env.DB_HOST,
-//         dialect: 'mysql',
-//         port: env.DB_PORT,
-//         logging: console.log,
-//         pool: {
-//             max: 10,
-//             min: 0,
-//             idle: 60000,
-//             acquire: 60000,
-//             evict: 30000
-//         }
-//     }
-// );
-
-// module.exports = sequelize;
-
 const { Sequelize } = require("sequelize");
 const env = require("./env");
 
--- src/index.js
+++ src/index.js
@@ -1,2 +1,12 @@
 const app = require("./app");
+
+if (require.main === module) {
+    const env = require("./config/env");
+    app.listen(env.VITE_FRONTEND_PORT, () => {
+        console.log(
+            `HRIS Employee Management running on port: ${env.VITE_FRONTEND_PORT}`
+        );
+    });
+}
+
 module.exports = app;
