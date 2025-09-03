ef788be9845a9a874aee1068f529246a62802c78
jzaragosa06 <junjunzaragosa2309@gmail.com>
2025-09-03 11:28:07 +0800

    chore: revert change to index.js

--- src/index.js
+++ src/index.js
@@ -1,19 +1,6 @@
-// const app = require("./app");
-// const env = require("./config/env");
-
-// app.listen(env.VITE_FRONTEND_PORT, () => {
-//     console.log(`HRIS Employee Management runing on port: ${env.VITE_FRONTEND_PORT}: access on localhost:${env.VITE_FRONTEND_PORT}`)
-// });
-
 const app = require("./app");
+const env = require("./config/env");
 
-if (require.main === module) {
-    const env = require("./config/env");
-    app.listen(env.VITE_FRONTEND_PORT, () => {
-        console.log(
-            `HRIS Employee Management running on port: ${env.VITE_FRONTEND_PORT}`
-        );
-    });
-}
-
-module.exports = app;
+app.listen(env.VITE_FRONTEND_PORT, () => {
+    console.log(`HRIS Employee Management runing on port: ${env.VITE_FRONTEND_PORT}: access on localhost:${env.VITE_FRONTEND_PORT}`)
+});
