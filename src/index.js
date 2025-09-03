01438495c85599eb1c2339ac7caad75382e51de1
jzaragosa06 <junjunzaragosa2309@gmail.com>
2025-09-03 11:40:02 +0800

    chore: revert

--- src/index.js
+++ src/index.js
@@ -1,9 +1,6 @@
-// const app = require("./app");
-// const env = require("./config/env");
+const app = require("./app");
+const env = require("./config/env");
 
-// app.listen(env.VITE_FRONTEND_PORT, () => {
-//     console.log(`HRIS Employee Management runing on port: ${env.VITE_FRONTEND_PORT}: access on localhost:${env.VITE_FRONTEND_PORT}`)
-// });
-
-const app = require('../src/app');
-module.exports = app;
+app.listen(env.VITE_FRONTEND_PORT, () => {
+    console.log(`HRIS Employee Management runing on port: ${env.VITE_FRONTEND_PORT}: access on localhost:${env.VITE_FRONTEND_PORT}`)
+});
