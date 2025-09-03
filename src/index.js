3927257f8c9e2b2cf453c2f0d076d774096ba763
jzaragosa06 <junjunzaragosa2309@gmail.com>
2025-09-03 11:36:57 +0800

    fix: fix deployment

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
-const app = require("./app");
-const env = require("./config/env");
+// const app = require("./app");
+// const env = require("./config/env");
 
-app.listen(env.VITE_FRONTEND_PORT, () => {
-    console.log(`HRIS Employee Management runing on port: ${env.VITE_FRONTEND_PORT}: access on localhost:${env.VITE_FRONTEND_PORT}`)
-});
+// app.listen(env.VITE_FRONTEND_PORT, () => {
+//     console.log(`HRIS Employee Management runing on port: ${env.VITE_FRONTEND_PORT}: access on localhost:${env.VITE_FRONTEND_PORT}`)
+// });
+
+const app = require('../src/app');
+module.exports = app;
