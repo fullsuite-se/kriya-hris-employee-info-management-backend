efa83637e6ec5f67ed7ec19385fcedc16e3e313f
jzaragosa06 <junjunzaragosa2309@gmail.com>
2025-09-03 11:11:21 +0800

    fix: export app instead of listening to changes for production

--- src/index.js
+++ src/index.js
@@ -1,6 +1,19 @@
-const app = require("./app");
-const env = require("./config/env");
+// const app = require("./app");
+// const env = require("./config/env");
 
-app.listen(env.VITE_FRONTEND_PORT, () => {
-    console.log(`HRIS Employee Management runing on port: ${env.VITE_FRONTEND_PORT}: access on localhost:${env.VITE_FRONTEND_PORT}`)
-});
+// app.listen(env.VITE_FRONTEND_PORT, () => {
+//     console.log(`HRIS Employee Management runing on port: ${env.VITE_FRONTEND_PORT}: access on localhost:${env.VITE_FRONTEND_PORT}`)
+// });
+
+const app = require("./app");
+
+if (require.main === module) {
+    const env = require("./config/env");
+    app.listen(env.VITE_FRONTEND_PORT, () => {
+        console.log(
+            `HRIS Employee Management running on port: ${env.VITE_FRONTEND_PORT}`
+        );
+    });
+}
+
+module.exports = app;
