be9d5f5749f0d4de3edcf5c7ba8a6a3eabcbb194
allenjalvaro <allenjalvaro@gmail.com>
2025-09-10 09:39:40 +0800

    fix: adjust sequelize pool configuration and improve connection settings

--- src/config/db.js
+++ src/config/db.js
@@ -3,23 +3,30 @@ const env = require("./env");
 
 const sequelize = new Sequelize(
   env.DB_NAME_DEV,
-    env.DB_USER,
-    env.DB_PASSWORD,
-    {
-        host: process.env.DB_HOST,
-        port: process.env.DB_PORT || 3306,
-        dialect: "mysql",
-        dialectModule: require("mysql2"),
-        logging: false,
+  env.DB_USER,
+  env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT || 3306,
+    dialect: "mysql",
+    dialectModule: require("mysql2"),
+    logging: false,
 
-        pool: {
-            max: 5,
-            min: 0,
-            acquire: 30000,
-            idle: 10000,
-        },
-    }
+    pool: {
+      max: 5,
+      min: 0,
+      acquire: 60000, 
+      idle: 10000,  
+    },
+
+    dialectOptions: {
+      connectTimeout: 20000, 
+      ssl: {
+        require: true,       
+        rejectUnauthorized: false, 
+      },
+    },
+  }
 );
-
 
 module.exports = sequelize;
