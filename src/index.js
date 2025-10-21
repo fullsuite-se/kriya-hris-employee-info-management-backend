09e3bddbed5327b1bc4903a433ae3ec18559b56e
allenjalvaro <allenjalvaro@gmail.com>
2025-10-21 13:50:37 +0800

    fix: Update index.js to include VERCEL environment check and tidy up console log formatting

--- src/index.js
+++ src/index.js
@@ -1,13 +1,11 @@
 const app = require("./app");
 
-if (require.main === module) {
+if (process.env.VERCEL !== "1" && require.main === module) {
     const env = require("./config/env");
-
     const PORT = process.env.PORT || env.VITE_FRONTEND_PORT;
+
     app.listen(PORT, () => {
-        console.log(
-            `HRIS Employee Management running on port: ${PORT}`
-        );
+        console.log(`HRIS Employee Management running on port: ${PORT}`);
     });
 }
 
